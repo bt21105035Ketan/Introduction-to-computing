695c70f7d3d040a5add37079ef141ab5dcc7a5c6
bt21105035Ketan <96742020+bt21105035Ketan@users.noreply.github.com>
2022-01-05 21:26:55 +0530

    Add files via upload

---
+++ ques1.py
@@ -0,0 +1,14 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# taking input from user
+first_number = float(input("Enter first number:"))
+Enter first number: 10.0
+second_number = float(input("Enter second number:"))
+Enter second number: 20.0
+third_number = float(input("Enter third number:"))
+Enter third number: 30.0
+# average of three numbers
+average = ((first_number + second_number + third_number)/3)
+# printing the average
+print("The average of three numbers :", average)
+The average of three numbers : 20.0
