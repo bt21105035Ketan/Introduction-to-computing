c026d47c521b497959ceb726a345d3e5b6942f9a
bt21105035Ketan <96742020+bt21105035Ketan@users.noreply.github.com>
2022-02-12 20:52:42 +0530

    Add files via upload

---
+++ Assignment 3/output_assignment3.py
@@ -0,0 +1,91 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========= RESTART: C:/Users/Ketan/Desktop/Assignment 3/ques1 assign3.py ========
+Enter your string: Balloon
+b : 1
+a : 1
+l : 2
+o : 2
+n : 1
+
+========= RESTART: C:/Users/Ketan/Desktop/Assignment 3/ques1 assign3.py ========
+Enter your string: Ketan Mattoo ketan mattoo
+ketan : 2
+mattoo : 2
+
+========= RESTART: C:/Users/Ketan/Desktop/Assignment 3/ques2 assign3.py ========
+Enter the year: 2000
+Enter the month number(1-12): 2
+Enter the date: 29
+DD/MM/YYYY: 1/3/2000
+
+========= RESTART: C:/Users/Ketan/Desktop/Assignment 3/ques2 assign3.py ========
+Enter the year: 2003
+Enter the month number(1-12): 7
+Enter the date: 26
+DD/MM/YYYY: 27/7/2003
+
+========= RESTART: C:/Users/Ketan/Desktop/Assignment 3/ques3 assign3.py ========
+Enter the number of elements you want in list 5
+Enter the number 2
+Enter the number 8
+Enter the number 4
+Enter the number 10
+Enter the number 15
+Your output is: [(2, 4), (8, 64), (4, 16), (10, 100), (15, 225)]
+
+========= RESTART: C:/Users/Ketan/Desktop/Assignment 3/ques4 assign3.py ========
+Enter the Grade Point: 9
+Your grade is 'A' and Excellent performance.
+
+========= RESTART: C:/Users/Ketan/Desktop/Assignment 3/ques5 assign3.py ========
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+========= RESTART: C:/Users/Ketan/Desktop/Assignment 3/ques6 assign3.py ========
+Enter the name: Ketan
+Enter the eight digit SID: 21105035
+Would you like to enter more details? Y/N : Y
+Enter the name: Himanshu
+Enter the eight digit SID: 21105050
+Would you like to enter more details? Y/N : Y
+Enter the name: Aayush
+Enter the eight digit SID: 21105001
+Would you like to enter more details? Y/N : N
+
+part(a)
+21105035 is Ketan
+21105050 is Himanshu
+21105001 is Aayush
+
+part(b)
+dictionary sorted by names: ['Aayush', 'Himanshu', 'Ketan']
+
+part(c)
+dictionary sorted by SID: [21105001, 21105035, 21105050]
+
+part(d)
+Enter the SID of the student whose detail you want : 21105035
+The student name is :  Ketan
+
+========= RESTART: C:/Users/Ketan/Desktop/Assignment 3/ques7 assign3.py ========
+Enter your number: 10
+The fibonacci sequence is: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+The average is: 8.8
+
+========= RESTART: C:/Users/Ketan/Desktop/Assignment 3/ques8 assign3.py ========
+
+(a) elements in set1 and set2 but not in both: {1, 3, 5, 6, 8}
+
+(b) elements only in any one of the three set: {17, 3, 6, 8, 9, 13}
+
+(c) elements only in any one of the three set: {1, 2, 4, 5}
+
+(d) elements in the range 1 to 10 and not in set1: {6, 7, 8, 9, 10}
+
+(e) elements in the range 1 to 10 but not in set1, set2 and set3: {10, 7}
